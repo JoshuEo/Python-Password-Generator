19ceceb8e07c016c74e6be6982a7f0f677c393ac
JoshuEo <joshuaeom132@gmail.com>
2021-02-16 16:15:54 -0500

    Second Commit 2/16

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+
+
+### Todo list:
+- Add a try..except block in if __name__ method
+- Find a way to cleanup the code
+ - Creating a method for checking user input and then returning their value
+- Make truely random characters part of the code 
+### Questions:
+
+### Extra (If I end up finishing this project):
+- Ask for a word - then we can translate it into something a little stronger
--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,19 +1,19 @@
-"""
-Todo list:
-- Add a try..except block in if __name__ method
-Questions:
-- How do you have variables and call them through methods? (talking about UPPER, LOWER, )
-Extra:
-- Ask for a word - then we can translate it into something a little stronger
-"""
 import random
 
 
 class PasswordGenerator:
 
+    UPPER = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    LOWER = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    SPECIAL = ["!", """, "#", "$", "%", "&", """, "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+    chartype = [UPPER, LOWER, NUMBERS, SPECIAL]
+
     # Instance variables
     def __init__(self):
         self.length = 0
+        self.upper = False
+        self.lower = False
         self.number = False
         self.special = False
         self.chars = []
@@ -23,48 +23,65 @@ class PasswordGenerator:
         print("-" * 40)
         print("Please enter the necessary characteristics you want the password to have:")
 
+    def checkInput(self, value):
+        if value.upper() == "Y" or value.upper() == "YES":
+            return True
+        elif value.upper() == "N" or value.upper() == "NO":
+            return False
+        else:
+            print("Please enter valid inputs (Either \"yes\" or \"no\")")
+
     def setLength(self):
         size = int(input("Length: "))
         self.length = size
 
+    def setUpper(self):
+        addupper = input("Uppercase Letters (Y/N): ")
+        self.upper = self.checkInput(addupper)
+
+    def setLower(self):
+        addlower = input("Lowercase Letters (Y/N): ")
+        self.lower = self.checkInput(addlower)
+
     def setNumber(self):
         addnumber = input("Add numbers? (Y/N): ")
-        if addnumber.upper == "Y" or addnumber.upper == "YES":
-            self.number = True
-        elif addnumber.upper == "N" or addnumber.upper == "NO":
-            self.number = False
+        self.number = self.checkInput(addnumber)
 
     def setSpecial(self):
         addspecial = input("Add special characters? (Y/N): ")
-        if addspecial.upper == "Y" or addspecial.upper == "YES":
-            self.special = True
-        elif addspecial.upper == "N" or addspecial.upper == "NO":
-            self.special = False
+        self.special = self.checkInput(addspecial)
 
     def chooseChars(self):
-        pass
+        usersCharacteristics = [self.upper, self.lower, self.number, self.special]
+        index = 0
+        for types in usersCharacteristics:
+            if types == True:
+                self.chars.append(self.chartype[index])
+                index += 1
+            else:
+                index += 1
+                continue
 
     def generate(self):
-        # List of characters
-        UPPER = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-        LOWER = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        SPECIAL = ["!", """, "#", "$", "%", "&", """, "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
-        types = [UPPER, LOWER, NUMBERS, SPECIAL]
         newpass = ""
-        
-        for char in range(0, self.length):
-            whichtype = random.choice(types)
-            char = random.choice(whichtype)
-            newpass = newpass + char
+        for character in range(0, self.length):
+            whichtype = random.choice(self.chars)
+            character = random.choice(whichtype)
+            newpass = newpass + character
         return newpass
 
 
 if __name__ == "__main__":
-    # Create an Password Generator Object
-    password = PasswordGenerator()
-    password.welcome()
-    password.setLength()
-    password.setNumber()
-    password.setSpecial()
-    print(f"Generated Password: {password.generate()}")
+    try:
+        # Create an Password Generator Object -> Running all operations
+        password = PasswordGenerator()
+        password.welcome()
+        password.setLength()
+        password.setLower()
+        password.setUpper()
+        password.setNumber()
+        password.setSpecial()
+        password.chooseChars()
+        print(f"Generated Password: {password.generate()}")
+    except:
+        print("Please enter valid inputs (Either \"yes\" or \"no\"")
